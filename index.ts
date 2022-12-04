fd1ff252ce619f4c0584c10b84a04dd26fc27aa4
Maxim Chernykh <max_kray@list.ru>
2022-12-05 00:16:45 +0400

    auth method

---
+++ index.ts
@@ -0,0 +1,87 @@
+import express, { Request, Response } from "express";
+import { AuthReport, Error } from "./types";
+
+const app = express();
+
+// GET getUserByUsernameUrl
+// GET getUserByLoginUrl
+// GET getUserByUUIDUrl
+// GET getUserByTokenUrl
+// POST authorizeUrl
+// POST refreshTokenUrl
+// POST updateServerIdUrl
+// POST joinServerUrl
+// POST checkServerUrl
+
+// Методы getUserByUsernameUrl, getUserByLoginUrl, getUserByUUIDUrl, checkServerUrl
+// ожидают ответ типа HttpUser (См. Список объектов), а данные получают в виде GET параметров
+// Метод getUserByTokenUrl ожидает ответ типа HttpUserSession (См. Список объектов)
+// Методы authorizeUrl, refreshTokenUrl ожидают ответ типа AuthReport (См. Список объектов)
+// Методы joinServerUrl,updateServerIdUrl ожидают ответ 200 в случае успеха
+
+interface AuthorizeUrlReq {
+  login: string;
+  context: {
+    ip: string;
+  };
+  password: {
+    password: string;
+    type: string;
+  };
+  minecraftAccess: boolean;
+  //если false, то в AuthReport возвращать minecraftAccessToken не требуется
+}
+
+app.get(
+  "/authorizeUrl",
+  (
+    req: Request<any, any, AuthorizeUrlReq>,
+    res: Response<AuthReport | Error>
+  ) => {
+    const { login, context, password, minecraftAccess } = req.body;
+
+    if (!login && !password)
+      return res.status(200).json({ error: "auth.wrongpassword" });
+
+    if (login === "MiXeR54" && password.password === "test")
+      return res.status(200).json({
+        minecraftAccessToken: "",
+        oauthAccessToken: "",
+        oauthRefreshToken: "",
+        oauthExpire: 0,
+        session: {
+          id: "",
+          user: {
+            username: login,
+            uuid: "",
+            accessToken: "",
+            properties: {
+              test: "",
+            },
+            permissions: {
+              perms: [],
+              roles: [],
+            },
+            assets: {
+              SKIN: {
+                url: "",
+                digest: "",
+                metadata: {
+                  model: "",
+                },
+              },
+              CAPE: {
+                url: "",
+                digest: "",
+              },
+            },
+          },
+          expireIn: 0,
+        },
+      });
+  }
+);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
